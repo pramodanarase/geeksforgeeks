0481d97385d7d5f9999dabc39edc1ddecfa6e073
Pramod Anarase <pramodanarase@gmail.com>
2018-10-03 18:21:44 +0530

    added algorithm strategy

---
+++ branch_and_bound/src/main/java/org/branch_and_bound/App.java
@@ -0,0 +1,13 @@
+package org.branch_and_bound;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ brute_force/src/main/java/org/brute_force/App.java
@@ -0,0 +1,13 @@
+package org.brute_force;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
