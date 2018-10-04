cd3e624d3e3069abf1c0011e8c55785f7476c384
Pramod Anarase <pramodanarase@gmail.com>
2018-10-04 11:38:22 +0530

    added binary search

--- divide_and_conquer/iterative/src/main/java/org/iterative/IterativeBinarySearch.java
+++ divide_and_conquer/iterative/src/main/java/org/iterative/IterativeBinarySearch.java
@@ -2,4 +2,43 @@ package org.iterative;
 
 public class IterativeBinarySearch {
 
+	public static void main(String[] args) {
+		// data is sorted
+		int[] arr = { 1, 2, 4, 5, 8, 9, 14 };
+
+		int index = binarySearch(arr, 15);
+		if (index != -1) {
+			System.out.println("element found at " + index);
+		}else {
+			System.out.println("element not found");
+		}
+
+	}
+
+	private static int binarySearch(int[] arr, int key) {
+
+		int n = arr.length;
+
+		int l = 0;
+		int h = n;
+		int m = 0;
+		int itr =0;
+		while (l < h) {
+			m = (l + h) / 2;
+			if (arr[m] == key) {
+				System.out.println("iteration are :"+ itr);
+				return m;
+			} else {
+				if (arr[m] > key)
+					h = m-1;
+				else
+					l = m + 1;
+			}
+			itr ++;
+		}
+
+		System.out.println("iteration are :"+ itr);
+		return -1;
+	}
+
 }
--- divide_and_conquer/recursive/src/main/java/org/recursive/RecursiveBinarySearch.java
+++ divide_and_conquer/recursive/src/main/java/org/recursive/RecursiveBinarySearch.java
@@ -2,4 +2,41 @@ package org.recursive;
 
 public class RecursiveBinarySearch {
 
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 5, 7, 8, 12 };
+
+		int index = binarySearch(arr, 12);
+		if (index != -1) {
+			System.out.println("element found at: " + index);
+		} else {
+			System.out.println("element not found ");
+		}
+	}
+
+	private static int binarySearch(int[] arr, int key) {
+
+		int l = 0;
+		int h = arr.length-1;
+
+		return binarySearch(arr, l, h, key);
+	}
+
+	private static int binarySearch(int[] arr, int l, int h, int key) {
+
+		int m = (l + h) / 2;
+		if (m >= l && m <= h) {
+			if (arr[m] == key) {
+				return m;
+			}
+
+			if (arr[m] > key) {
+				// key on right side
+				return binarySearch(arr, l, m - 1, key);
+			} else {
+				return binarySearch(arr, m + 1, h, key);
+			}
+		}
+		return -1;
+	}
+
 }
